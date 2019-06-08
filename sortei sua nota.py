bf478a11389153b90925a217d3760541fee371dc
wakandavive <50594124+wakandavive@users.noreply.github.com>
2019-06-07 21:59:49 -0300

    sortei sua nota
    sortei sua nota

---
+++ sortei sua nota.py
@@ -0,0 +1,22 @@
+from random import randint
+
+
+notas1 = randint(0, 10)
+notas2 = randint(0, 10)
+
+
+
+print('Notas 1:,', notas1)
+print('Notas 2:,', notas2)
+
+
+
+
+soma_divide = notas1  + notas2 / 2
+
+média_para_passar = 7
+
+if soma_divide > média_para_passar:
+    print('aprovado')
+else:
+    print('reprovado')
