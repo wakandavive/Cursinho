52079dcbf64217cea2ef7ac754255ba66f3ae39a
wakandavive <50594124+wakandavive@users.noreply.github.com>
2019-08-13 20:47:17 -0300

    respeite o cod
    o pai é rato

---
+++ age/modulos.py
@@ -0,0 +1,96 @@
+def dia1():
+    dias_semana = input("quer saber oq tem pra segunda?:")
+
+    vari1 = "sim"
+
+    sim_dia1 = "sim"
+    horários = ["prog,18_00as18_30","mat,18_30as19_00","fisi,1930"]
+    lista_afazeres = ['estudar prog','estudar matematica','estudar física']
+
+    if dias_semana == sim_dia1:
+        print(/033[0,31;43"para segunda tem", lista_afazeres)
+    
+        
+
+    horário_pergunta = input("quer saber o horário de estudos:")
+    if horário_pergunta == vari1:
+        print(horários)
+    elif horário_pergunta != vari1:
+        print("blz,tem uma boa tarde")
+
+def dia2():
+    sim_dia2 = 'sim'
+    horários = ["prog,18_00as18_30","mat,18_30as19_00","fisi,1930"]
+    lista_afazeres_terca = ['estudar bio','estudar filo','estudar port']
+    vari1 = "sim"
+    dias_semana = input("quer saber oq tem pra terca?:")
+    if dias_semana == sim_dia2:
+        print("para terca tem", lista_afazeres_terca)
+    
+    horário_pergunta = input("quer saber o horário de estudos:")
+    if horário_pergunta == vari1:
+        print(horários)
+    elif horário_pergunta != vari1:
+        print("blz,tem uma boa tarde")
+
+
+def dia3():
+    sim_dia3 = "sim"
+    horários = ["prog,18_00as18_30","mat,18_30as19_00","fisi,1930"]
+    lista_afazeres_terca = ['estudar quimi','estudar geo','estudar artKKK']
+    vari1 = "sim"
+    dias_semana = input("quer saber oq tem pra quarta?:")
+    if dias_semana == sim_dia3:
+        print("para quarta tem", lista_afazeres_terca)
+    
+    horário_pergunta = input("quer saber o horário de estudos:")
+    if horário_pergunta == vari1:
+        print(horários)
+    elif horário_pergunta != vari1:
+        print("blz,tem uma boa tarde")
+
+
+ 
+def dia4():
+    sim_dia4 = 'sim'
+    horários = ["prog,18_00as18_30","mat,18_30as19_00","fisi,1930"]
+    lista_afazeres_terca = ['estudar his','estudar fis','estudar prog']
+    vari1 = "sim"
+    dias_semana = input("quer saber oq tem pra quinta?:")
+    if dias_semana == sim_dia4:
+        print("para quinta tem", lista_afazeres_terca)
+    
+    horário_pergunta = input("quer saber o horário de estudos:")
+    if horário_pergunta == vari1:
+        print(horários)
+    elif horário_pergunta != vari1:
+        print("blz,tem uma boa tarde")
+
+
+
+
+def dia5():
+    sim_dia5 = 'sim'
+    horários = ["prog,18_00as18_30","mat,18_30as19_00","fisi,1930"]
+    lista_afazeres_terca = ['estudar mat','estudar web','estudar bio']
+    vari1 = "sim"
+    dias_semana = input("qual quer saber oq tem pra sexta?:")
+    if dias_semana == sim_dia5:
+        print("para terca tem", lista_afazeres_terca)
+    
+    horário_pergunta = input("quer saber o horário de estudos:")
+    if horário_pergunta == vari1:
+        print(horários)
+    elif horário_pergunta != vari1:
+        print("blz,tem uma boa tarde")
+
+        
+
+
+
+
+
+
+                
+
+
---
+++ age/rodaragenda.py
@@ -0,0 +1,8 @@
+import modulos
+print("-_-_-__AGÊNDA ELETRÔNICA__-_-_-")
+modulos.dia1()
+modulos.dia2()
+modulos.dia3()
+modulos.dia4()
+modulos.dia5()
+
